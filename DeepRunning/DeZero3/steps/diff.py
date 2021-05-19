2d532e7652b2b99f6b92154bd95392b924a44ab6
kjmin622 <null>
2021-05-19 14:20:40 +0000

    DeZero

---
+++ DeepRunning/DeZero3/steps/diff.py
@@ -0,0 +1,13 @@
+from function import Variable, Square
+import numpy as np
+
+
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2*eps)
+
+
+print(numerical_diff(Square(), Variable(np.array(2.0))))
