40315bed18aac0db539f93b006d8358004f5be06
kjmin622 <null>
2021-06-01 14:23:45 +0000

    algorithm

--- algorithm/16637.cpp
+++ algorithm/16637.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<cstring>
-#include<stack>
 #include<vector>
 #include<queue>
 using namespace std;
@@ -35,40 +34,36 @@ void combi(int c){
 int main(){
 	cin>>n>>s;
 	combi(1);
-	
 	lli maxi = -2e31;
 	
-	//1*2+3*4*5-6*7*8*9*0
-	// 1     7   11  15
-	//2 + 3 * 20 - 42 * 72 * 0
 	for(vector<int> comb : com){
-		vector<lli> num;
+		queue<lli> num;
 		queue<char> calq;
 		int idx = 0;
 		bool flag = false;	
 		for(int i=1; i<n; i+=2){
 			if(!comb.empty() && !(comb.size()<=idx) && comb[idx]==i){
 				char cal = s[i];
-				if(cal == '+') num.push_back((s[i-1]-'0')+(s[i+1]-'0'));
-				if(cal == '-') num.push_back((s[i-1]-'0')-(s[i+1]-'0'));
-				if(cal == '*') num.push_back((s[i-1]-'0')*(s[i+1]-'0'));
+				if(cal == '+') num.push((s[i-1]-'0')+(s[i+1]-'0'));
+				if(cal == '-') num.push((s[i-1]-'0')-(s[i+1]-'0'));
+				if(cal == '*') num.push((s[i-1]-'0')*(s[i+1]-'0'));
 				idx++;
 				flag = true;
 			}
 			else{
-				if(!flag) num.push_back(s[i-1]-'0');
+				if(!flag) num.push(s[i-1]-'0');
 				calq.push(s[i]);
 				flag = false;
 				
 			}
 		}
-		if(!flag) num.push_back(s[n-1]-'0');
+		if(!flag) num.push(s[n-1]-'0');
 		
 		idx = 0;
-		int x = num[idx++]; 
+		int x = num.front(); num.pop(); 
 		while(!calq.empty()){
 			char ca = calq.front(); calq.pop();
-			int y = num[idx++];
+			int y = num.front(); num.pop();
 			if(ca == '+') x+=y;
 			if(ca == '-') x-=y;
 			if(ca == '*') x*=y;
@@ -77,5 +72,3 @@ int main(){
 	}
 	cout<<maxi<<endl;
 }
-// 19
-// 1*2+3*4*5-6*7*8*9*0
